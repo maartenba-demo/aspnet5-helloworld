bda4e7df6f16df6ffa377b819d0744b2938e0dad
Maarten Balliauw <maarten@maartenballiauw.be>
2014-12-12 18:16:50 +0000

    Added tests project

---
+++ sources/HelloWorld/test/HelloWorld.Core.Tests/HelloTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace HelloWorld.Core.Tests
+{
+    public class HelloTests
+    {
+        [Fact]
+        public void WorldReturnsCorrectString()
+        {
+
+        }
+    }
+}
